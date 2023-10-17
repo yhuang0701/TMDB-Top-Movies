56510105502d306f1a9992dc8da0f4e2d259e17f
Yichen Huang <yichen41@illinois.edu>
2023-10-17 14:22:17 -0500

    Added filter functionality to gallery view page and CSS effects for buttons

--- src/components/galleryView/Gallery.jsx
+++ src/components/galleryView/Gallery.jsx
@@ -1,14 +1,57 @@
 import './Gallery.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Gallery = ({ movieData }) => {
+const Gallery = ({ movieData, api_key }) => {
+  const [genres, setGenres] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
+  useEffect(() => {
+    getGenreData();
+    setFilteredMovies(movieData);
+  }, [movieData]);
+
+  const getGenreData = async () => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${api_key}`);
+      setGenres(response.data.genres);
+    } catch (error) {
+      console.error('Error fetching genres:', error);
+    }
+  }
+
+  const handleFilter = (genreId) => {
+    if (genreId === 0) {
+      setFilteredMovies(movieData);
+    } else {
+      const filteredMovieData = movieData.filter(movie => movie.genre_ids.includes(genreId))
+      setFilteredMovies(filteredMovieData);
+    }
+  }
+
+
   return (
-    <div id="gallery">
-      {movieData.map(movie => (
-        <div key={movie.id} className="movie-card">
-          <img className="movieImg" src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`} alt="movie image" />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="genreBtns">
+        <button className="genreBtn" onClick={() => handleFilter(0)}>All</button>
+        {genres.map(genre => (
+          <button
+            key={genre.id}
+            className="genreBtn"
+            onClick={() => handleFilter(genre.id)}>
+            {genre.name}
+          </button>
+        ))}
+      </div>
+
+      <div id="gallery">
+        {filteredMovies.map(movie => (
+          <div key={movie.id} className="movie-card">
+            <img className="movieImg" src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`} alt="movie image" />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 export default Gallery;
